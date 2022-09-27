03719525ccfcc8715f9de3497e9e613cc1c27920
Asif Anower <62245915+asif-aiub18@users.noreply.github.com>
2022-09-27 23:59:39 +0600

    team, contact and about page added.

--- APWT TASK_1/Lab1/routes/web.php
+++ APWT TASK_1/Lab1/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','App\Http\Controllers\HomeController@home');
 Route::get('/home', 'App\Http\Controllers\HomeController@home');
 Route::get('/product/service', 'App\Http\Controllers\HomeController@service');
 

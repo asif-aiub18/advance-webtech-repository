c7d67189efc3c66aab88f3e8bb9bab53c56f87a5
Asif Anower <62245915+asif-aiub18@users.noreply.github.com>
2022-09-27 23:30:02 +0600

    home and product service page added

---
+++ APWT TASK_1/Lab1/resources/views/page/index/landingPage.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card card-chart">
+            <div class="card-header ">
+                <div class="row">
+                    <div class="col-sm-6 text-left">
+                        <h2 class="card-title">DASHBOARD</h2>
+                        <h4 class="card-title">Balance :1000</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>       
---
+++ APWT TASK_1/Lab1/resources/views/product/service.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card card-chart">
+            <div class="card-header ">
+                <div class="row">
+                    <div class="col-sm-6 text-left">
+                        <h2 class="card-title">Prodcut Service</h2>
+                        <h4 class="card-title">Asif </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>       
